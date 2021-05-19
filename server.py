f2298d26ccd2068bfd9a1bb117ee3ba9c9263397
misfit11235 <catalin.abrasu@gmail.com>
2021-05-19 13:15:04 +0300

    get post data from route and return highlights array

--- server.py
+++ server.py
@@ -1,25 +1,24 @@
-# Import nessecary parts from flask and faker to generate random    # name and email.
 from flask import Flask, request, jsonify
 from faker import Faker
 from flask_cors import CORS
 import numpy as np
-# To create and initialize a faker generator.
+
 fake = Faker()
-# Create the app object that will route our calls.
 app = Flask(__name__)
 CORS(app)
-# Add a single endpoint that we can use as an API to accept GET and # POST requests.
-@app.route("/highlight", methods=["POST", "GET"])
+
+@app.route("/api/highlight", methods=["POST", "GET"])
 
 def computeHighlight():
+    meeting = request.get_json()
+    highlights = []
+    
+    for section in meeting["sections"]:
+        highlights.append(fake.paragraph(nb_sentences = 2, variable_nb_sentences = False))
 
-    highlight = {}
-    highlight["text"] = fake.paragraph(nb_sentences = 3, variable_nb_sentences = False)
-    highlight["type"] = "delimiter"
-
-    return jsonify(highlight)
-
-
+    print(highlights)
+    return jsonify(highlights)
+    
 # When run from command line, start the server.
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")

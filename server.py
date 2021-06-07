156df56035c19d15d64d5ff20448c7927c37a3b2
misfit11235 <catalin.abrasu@gmail.com>
2021-06-07 17:57:57 +0300

    receive deeptalk transcript and save to json

--- server.py
+++ server.py
@@ -2,6 +2,8 @@ from flask import Flask, request, jsonify
 from faker import Faker
 from flask_cors import CORS
 import numpy as np
+import time
+import json
 
 fake = Faker()
 app = Flask(__name__)
@@ -16,10 +18,20 @@ def computeHighlight():
     # for section in meeting["sections"]:
     #     highlights.append(fake.paragraph(nb_sentences = 2, variable_nb_sentences = False))
 
-    # print(highlights)
+    print(meeting)
     highlight = fake.paragraph(nb_sentences = 2, variable_nb_sentences = False)
+    time.sleep(2)
     return jsonify(highlight)
     
+@app.route("/api/deeptalk-transcript", methods=["POST"])
+
+def seeTranscript():
+    transcript = request.get_json()
+    print(transcript)
+    with open('../MeetingTool/static/transcript.json', 'w') as outfile:
+        json.dump(transcript, outfile)
+    return "ok"
+
 # When run from command line, start the server.
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
